4b47cf9ab022e9b9fa8491ddf35b78b4d2b64fc2
siddheshwarkhandare <235892541+siddheshwarkhandare@users.noreply.github.com>
2026-08-31 14:26:09 +0530

    add hash_password

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager,UserMixin,login_user,logout_user,login_requ
 from itsdangerous import URLSafeTimedSerializer
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash,check_password_hash
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime, timezone, timedelta
@@ -36,8 +37,14 @@ migrate=Migrate(app,db)
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(100),unique=True, nullable=False)
-    password=db.Column(db.String(150),nullable=False)
+    hash_password=db.Column(db.String(150),nullable=False)
     email = db.Column(db.String(150),unique=True,nullable=False)
+
+    def hash_generate(self,password):
+        self.hash_password=generate_password_hash(password)
+
+    def hash_check(self, password):
+        return check_password_hash(self.hash_password,password)
 class todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False) 
@@ -62,7 +69,7 @@ def login():
         password = request.form['password']
         user=User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and user.hash_check(password):
             login_user(user)
             return redirect('/')
         else:
@@ -81,16 +88,25 @@ def register():
         email = request.form['email']
         password=request.form['password']
         conpassword=request.form['confirm_password']
-        user=User.query.filter_by(username=username).first()
 
         if password!= conpassword:
             flash("Password not  match")
             return redirect(url_for('register'))
+
+        if User.query.filter_by(username=username).first():
+            flash("Username already exist")
+            return redirect(url_for('/register'))
+        
+        if  User.query.filter_by(email=email).first():
+            flash("Email already exist")
+            return redirect(url_for('/register'))
+
+        
         user=User(
             username=username,
             email=email,
-            password=password
         )
+        user.hash_generate(password)
         db.session.add(user)
         db.session.commit()
         return redirect('/')
